be2ba9a38b3b255f2e3c98b4db6729af8c3dc45c
gkarandin <gkarandin@gmail.com>
2023-12-05 20:59:58 +0300

    Change balls rait and more type nominations and other

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
+const active1Color = "#7a2073";
+const active2Color = "#b36424";
+const active3Color = "#cc7810";
+const active4Color = "#db8b00";
+const active5Color = "orange";
+
 function App() {
 
   const participants: any = [];
@@ -8,18 +14,38 @@ function App() {
   rating.forEach((participant) => {
     const dancer = {
       name: "",
+      surname: "",
       points: 0,
     };
-    dancer.name = participant.firstName
+    dancer.name = participant.firstName;
+    dancer.surname = participant.lastName;
 
     participant.result.forEach((res) => {
-      const nominationType = res.nomination[0].type.code;
-      switch (nominationType) {
+      const nomination = res.nomination[0];
+      if (!nomination || !nomination.type) return;
+      const code = nomination.type.code;
+      switch (code) {
+        case "JNJ_NEWCOMER":
         case "JNJ_NOVICE":
+        case "JNJ_INTERMEDIATE":
+        case "JNJ_ADVANCED":
+        case "JNJ_ALLSTAR":
+        case "JNJ_SOPHISTICATED":
+        case "JNJ_AMPRO":
+        case "JNJ_PROAM":
+        case "JNJ_JACKPOT":
           dancer.points += 10;
           break;
         case "STRICTLY_NOVICE":
+        case "STRICTLY_INTERMEDIATE":
+        case "STRICTLY_OPEN":
           dancer.points += 20;
+          break;
+        case "ROUTINE":
+          dancer.points += 50;
+          break;
+        case "SHOW":
+          dancer.points += 60;
           break;
         default:
       }
@@ -31,8 +57,6 @@ function App() {
     return -1;
   };
   participants.sort(compare);
-
-  console.log("rating - ", participants)
 
   return (
     <div className="App">
@@ -49,6 +73,7 @@ function App() {
             <ol>
               <li>Баллы получают все</li>
               <li>В приоритете именно участие, а не выход в призы/финалы</li>
+              <li>Нет разделений на дивизионы. Нет разделений на роли: ученик/преподаватель, фоловер/лидер</li>
               <li>Баллы обнуляются ежегодно (с сохранением истории и занесением в зал и т.п...)</li>
             </ol>
           </div>
@@ -73,17 +98,29 @@ function App() {
             <li>10 баллов - JnJ</li>
             <li>20 баллов - Strictly</li>
             <li>50 баллов - Routine</li>
-            {/*<li>2 балла - вторая пятерка с конца (при наличии)</li>*/}
-            {/*<li>+1 балл - каждая следующая пятерка после второй с конца (при наличии)</li>*/}
+            <li>60 баллов - Show</li>
           </ol>
         </div>
         <div className="results">
-          <div>Учтены: Westie Fest JnJ-Novice, Strictly Novice*</div>
+          <div>Учтены: Westie Fest Gala 2023, WCS Утренник 2023*</div>
           <table>
           {
             participants.map((participant: any) => {
+              let color = "";
+              const { points, name } = participant;
+              if (points <= 30) {
+                color = active1Color;
+              } else if (points > 30 && points <= 100) {
+                color = active2Color;
+              } else if (points > 100 && points <= 250) {
+                color = active3Color;
+              } else if (points > 250 && points <= 400) {
+                color = active4Color;
+              } else {
+                color = active5Color;
+              }
               return (
-                <tr style={{backgroundColor:"#7a2073"}}>
+                <tr style={{backgroundColor: color}}>
                   <td>{participant.name}</td>
                   <td>{participant.points}</td>
                 </tr>
@@ -95,154 +132,28 @@ function App() {
             * Данные берутся из открытых источников. В данных могут содержаться ошибки.<br />
             Если нет данных по конкретному танцору: либо данные скрыты, либо ошибка.
           </div>
-
-
-          {/*</table><table>*/}
-          {/*  <tr style={{backgroundColor:"orange"}}>*/}
-          {/*    <td rowSpan={5}>Огонь!</td>*/}
-          {/*    <td>Маша</td>*/}
-          {/*    <td>54</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"orange"}}>*/}
-          {/*    <td>Денис</td>*/}
-          {/*    <td>52</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"orange"}}>*/}
-          {/*    <td>Сергей</td>*/}
-          {/*    <td>51</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"orange"}}>*/}
-          {/*    <td>Света</td>*/}
-          {/*    <td>50</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"orange"}}>*/}
-          {/*    <td>Надежда</td>*/}
-          {/*    <td>50</td>*/}
-          {/*  </tr>*/}
-
-          {/*  <tr style={{backgroundColor:"#db8b00"}}>*/}
-          {/*    <td rowSpan={5}>Невероятно активные</td>*/}
-          {/*    <td>Данила</td>*/}
-          {/*    <td>48</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#db8b00"}}>*/}
-          {/*    <td>Елена</td>*/}
-          {/*    <td>45</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#db8b00"}}>*/}
-          {/*    <td>Сергей</td>*/}
-          {/*    <td>40</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#db8b00"}}>*/}
-          {/*    <td>Дмитрий</td>*/}
-          {/*    <td>40</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#db8b00"}}>*/}
-          {/*    <td>Федор</td>*/}
-          {/*    <td>40</td>*/}
-          {/*  </tr>*/}
-
-          {/*  <tr style={{backgroundColor:"#cc7810"}}>*/}
-          {/*    <td rowSpan={5}>Чрезвычайно активные</td>*/}
-          {/*    <td>Никита</td>*/}
-          {/*    <td>38</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#cc7810"}}>*/}
-          {/*    <td>Валентина</td>*/}
-          {/*    <td>37</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#cc7810"}}>*/}
-          {/*    <td>Арина</td>*/}
-          {/*    <td>35</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#cc7810"}}>*/}
-          {/*    <td>Дмитрий</td>*/}
-          {/*    <td>30</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#cc7810"}}>*/}
-          {/*    <td>Иван</td>*/}
-          {/*    <td>30</td>*/}
-          {/*  </tr>*/}
-
-          {/*  <tr style={{backgroundColor:"#b36424"}}>*/}
-          {/*    <td rowSpan={5}>Очень активные</td>*/}
-          {/*    <td>Наташа</td>*/}
-          {/*    <td>27</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#b36424"}}>*/}
-          {/*    <td>Денис</td>*/}
-          {/*    <td>24</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#b36424"}}>*/}
-          {/*    <td>Сергей</td>*/}
-          {/*    <td>21</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#b36424"}}>*/}
-          {/*    <td>Дмитрий</td>*/}
-          {/*    <td>20</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#b36424"}}>*/}
-          {/*    <td>Федор</td>*/}
-          {/*    <td>20</td>*/}
-          {/*  </tr>*/}
-
-          {/*  <tr style={{backgroundColor:"#7a2073"}}>*/}
-          {/*    <td rowSpan={5}>Весьма активные</td>*/}
-          {/*    <td>Настя</td>*/}
-          {/*    <td>13</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#7a2073"}}>*/}
-          {/*    <td>Константин</td>*/}
-          {/*    <td>12</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#7a2073"}}>*/}
-          {/*    <td>Мила</td>*/}
-          {/*    <td>11</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#7a2073"}}>*/}
-          {/*    <td>Дмитрий</td>*/}
-          {/*    <td>10</td>*/}
-          {/*  </tr>*/}
-          {/*  <tr style={{backgroundColor:"#7a2073"}}>*/}
-          {/*    <td>Валерий</td>*/}
-          {/*    <td>10</td>*/}
-          {/*  </tr>*/}
-          {/*</table>*/}
-          {/*<h1>Манифест</h1>*/}
-          {/*<div>*/}
-          {/*  <ol>*/}
-          {/*    <li>Каждый участник сообщества уделяет занятию танцами столько времени и сил,*/}
-          {/*      сколько позволяют субъективные и объективные обстоятельства*/}
-          {/*    </li>*/}
-          {/*    <li>Каждый участник может желать получить оценку своих занятий относительно своих возможностей</li>*/}
-          {/*    <li>Любой из существующих инструментов оценки прогресса, лишь частично учитывает обстоятельства*/}
-          {/*      конкретного участника*/}
-          {/*    </li>*/}
-          {/*    <li>Данный индикатор только один из инструментов</li>*/}
-          {/*  </ol>*/}
-          {/*</div>*/}
         </div>
         <div className="results-legenda">
           <div>Легенда</div>
           <table>
             <tr style={{backgroundColor:"orange"}}>
               <td>Огонь!</td>
-              <td>150 баллов и более</td>
+              <td>400 баллов и более</td>
             </tr>
 
             <tr style={{backgroundColor:"#db8b00"}}>
               <td>Невероятно активные</td>
-              <td>70-150 баллов</td>
+              <td>до 400 баллов</td>
             </tr>
 
             <tr style={{backgroundColor:"#cc7810"}}>
               <td>Чрезвычайно активные</td>
-              <td>50-70 баллов</td>
+              <td>до 250 баллов</td>
             </tr>
 
             <tr style={{backgroundColor:"#b36424"}}>
               <td>Очень активные</td>
-              <td>30-50 баллов</td>
+              <td>до 100 баллов</td>
             </tr>
             <tr style={{backgroundColor:"#7a2073"}}>
               <td>Весьма активные</td>
